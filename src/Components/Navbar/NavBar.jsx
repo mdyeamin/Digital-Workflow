6c767e9949a50100c8551d713d71e69fb918a9d5
mdyeamin <ahmedyeamin05@gmail.com>
2026-03-31 00:53:50 +0600

    navigation responsive

--- src/Components/Navbar/NavBar.jsx
+++ src/Components/Navbar/NavBar.jsx
@@ -9,39 +9,87 @@ const navItems = [
   { id: 5, name: "FAQ", url: "/faq" },
 ];
 const navLinks = navItems.map((nav) => (
-  <li key={nav.id} className="md:border-none py-2 px-8 bg-gray-500">
-    <a href={nav.url}>{nav.name}</a>
+  <li key={nav.id} className="list-none group transition-all duration-300">
+    <a
+      href={nav.url}
+      className="
+      
+      block py-3 px-8 
+      bg-white text-black 
+      font-bold uppercase tracking-widest text-sm
+      border-b-2 border-transparent
+      transition-all duration-300
+      
+      
+      hover:text-[#4f38f6]
+      
+      group-hover:border-b-2 
+      group-hover:border-[#9315fa]
+      
+      md:inline-block md:py-0 md:px-4
+      md:bg-white md:text-black
+      md:relative md:overflow-hidden
+      
+      md:after:content-[''] md:after:absolute md:after:bottom-0 md:after:left-0 
+      md:after:w-full md:after:h-0.5 
+      md:after:bg-linear-to-r md:after:from-[#4f38f6] md:after:via-[#7922f8] md:after:to-[#9315fa]
+      md:after:transform md:after:scale-x-0 md:hover:after:scale-x-100
+      md:after:transition-transform md:after:duration-300
+    "
+    >
+      {nav.name}
+    </a>
   </li>
 ));
 
 const NavBar = () => {
   const [menu, setMenu] = useState(false);
-  console.log(menu);
   return (
     <>
       <div className="shadow w-full">
         <div className="mx-5 md:mx-10">
           <div className="max-w-7xl mx-auto flex justify-between items-center h-16">
-            <div className="flex items-center gap-5">
+            <div className="flex items-center gap-2">
               <button onClick={() => setMenu(!menu)}>
                 {menu ? (
-                  <IoCloseSharp className="md:hidden text-3xl" />
+                  <IoCloseSharp className="md:hidden text-xl" />
                 ) : (
-                  <GiHamburgerMenu className="md:hidden text-3xl  " />
+                  <GiHamburgerMenu className="md:hidden text-xl  " />
                 )}
               </button>
 
-              <ul className={`md:hidden absolute top-15 ${menu ? "left-0":"-left-40"} duration-500 space-y-2 bg-gray-700`}>
+              <ul
+                className={`md:hidden absolute top-16 ${menu ? "left-0" : "-left-50"} duration-500 space-y-2 shadow-md border border-gray-300`}
+              >
                 {navLinks}
-                </ul>
+              </ul>
 
-              <h4 className="text-4xl font-bold bg-linear-to-r from-[#4f38f6] via-[#7922f8] to-[#9315fa] bg-clip-text text-transparent">
+              <h4 className="text-xl lg:text-4xl font-bold bg-linear-to-r from-[#4f38f6] via-[#7922f8] to-[#9315fa] bg-clip-text text-transparent">
                 DigiTools
               </h4>
             </div>
-            <ul className="flex gap-8 hidden md:flex">{navLinks}</ul>
+            <ul className="flex hidden md:flex items-center">{navLinks}</ul>
             <div className="buttons">
-              <button>Click</button>
+              <button
+  className="
+    text-white px-5 py-2 font-bold uppercase tracking-widest text-[10px]
+    rounded-full 
+    bg-[linear-gradient(to_right,#4f38f6,#7922f8,#9315fa)]
+    bg-[length:200%_auto] 
+    bg-left
+    transition-all duration-500 ease-out
+    
+    md:px-4 md:py-2 md:text-xs
+    
+    lg:px-6 lg:py-3 lg:text-sm
+    
+    hover:bg-right
+    hover:scale-105 active:scale-95
+    shadow-lg hover:shadow-[#7922f8]/40
+  "
+>
+  Get Started
+</button>
             </div>
           </div>
         </div>
